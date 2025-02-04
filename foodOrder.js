82c6eab5119a974e34f310560c72c1600c84354d
karthikeya <122973946+karthikeya9900@users.noreply.github.com>
2025-02-04 22:26:23 +0530

    added display function

--- foodOrder.js
+++ foodOrder.js
@@ -1,43 +1,47 @@
 const timeElapsed = (startTime) =>
   `[${((Date.now() - startTime) / 1000).toFixed(2)}s]`;
 
+const display = (message, obj) => {
+  console.log(message, obj || "");
+};
+
 const receiveOrder = () => {
   return new Promise((resolve) => {
     const startTime = Date.now();
     const order = { orderId: 123, startTime, item: "burger" };
-    console.log(`[0.00s] Order received:`, order);
+    display("[0.00s] Order received:", order);
     resolve(order);
   });
 };
 
 const prepareFood = (order) => {
-  console.log(`${timeElapsed(order.startTime)} Preparing food...`);
+  display(`${timeElapsed(order.startTime)} Preparing food...`);
   return new Promise((resolve) => {
     setTimeout(() => {
-      console.log(`${timeElapsed(order.startTime)} Food is ready:`, order);
+      display(`${timeElapsed(order.startTime)} Food is ready:`, order);
       resolve(order);
     }, 3000);
   });
 };
 
 const packOrder = (order) => {
-  console.log(`${timeElapsed(order.startTime)} Packing order...`);
+  display(`${timeElapsed(order.startTime)} Packing order...`);
   return new Promise((resolve) => {
     setTimeout(() => {
       order.packageDetails = "Packed in eco-friendly box";
-      console.log(`${timeElapsed(order.startTime)} Order packed:`, order);
+      display(`${timeElapsed(order.startTime)} Order packed:`, order);
       resolve(order);
     }, 2000);
   });
 };
 
 const deliverOrder = (order) => {
-  console.log(`${timeElapsed(order.startTime)} Delivering order...`);
+  display(`${timeElapsed(order.startTime)} Delivering order...`);
   return new Promise((resolve) => {
     setTimeout(() => {
       order.deliveryDetails = "Delivered by John at 7:30 PM";
 
-      console.log(`${timeElapsed(order.startTime)} Order delivered:`, order);
+      display(`${timeElapsed(order.startTime)} Order delivered:`, order);
       resolve(order);
     }, 5000);
   });
